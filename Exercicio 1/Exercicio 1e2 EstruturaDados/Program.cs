002ca849de8d286861744dc85ee1f276455eecf8
Luiz Guilherme de Souza <l.guilhermedesouza@gmail.com>
2022-02-21 08:02:47 -0300

    Commit das pastas
    Número dos exercicios 1 e 2 de pilha realizados em C#

---
+++ Exercicio 1/Exercicio 1e2 EstruturaDados/Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio_1e2_EstruturaDados
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int opcao = 0;
+            Funcionario valor = new Funcionario();
+            Pilha minhaPilha = new Pilha(); // cria uma instância da classe pilha!
+          
+            do
+            {
+                try
+                {
+                    Console.Write("\n\n Escolha: 1-> empilha 2->desempilha " +
+                    " 3->topo 4-> tamanho   5->Listar  6-> Somar salários  7-> Listar os dados 8-> Somar salarios 9-> sair : ");
+                    opcao = Convert.ToInt32(Console.ReadLine());
+                    if (opcao == 1)
+                    {
+                        Funcionario novo = new Funcionario();
+                        Console.Write(">>Digite o valor que deseja empilhar em nome: ");
+                        valor.nome = Console.ReadLine();
+                        Console.Write(">>Digite o valor que deseja empilhar em salario: ");
+                        valor.salario = Convert.ToDouble(Console.ReadLine());
+                        minhaPilha.Empilha(valor);
+                    }
+                    else if (opcao == 2)
+                    {
+                        valor = minhaPilha.Desempilha();
+                        Console.WriteLine(">>Desempilhado: {0} \n\n", valor);
+                    }
+                    else if (opcao == 3)
+                    {
+                        valor = minhaPilha.RetornaTopo();
+                        Console.WriteLine(">>Valor no topo: {0} \n\n", valor);
+                    }
+                    else if (opcao == 4)
+                    {
+                        Console.WriteLine(">>Tamanho da pilha: {0}", minhaPilha.Tamanho());
+                    }
+                    else if (opcao == 9)
+                    {
+                        Console.WriteLine("Tchau!!!");
+                        // sai do programa
+                    }
+                    else if (opcao == 7) 
+                    {
+                        int qtd = 0;
+                        Funcionario[] dados = new Funcionario[minhaPilha.Tamanho()];
+                            
+                        for(int n = 0; n < (minhaPilha.Tamanho() + 1); n++) { 
+                            dados[n] = new Funcionario();
+                            valor = minhaPilha.Desempilha();
+                            dados[n] = valor;
+                            qtd++;
+                            }
+                        
+                        for(int aux = 0; aux < qtd; aux++) 
+                        {
+                            Console.WriteLine("Dados: ");
+                            Console.WriteLine("Nome: " + dados[aux].nome);
+                            Console.WriteLine("Salario: " + dados[aux].salario + "\n");
+                        }
+                        for(int empilha = 0; empilha < qtd; empilha++) 
+                        {
+                            minhaPilha.Empilha(dados[empilha]);
+                        }
+                       
+                    }
+                    else if (opcao == 8) 
+                    {
+                        Funcionario[] dados = new Funcionario[minhaPilha.Tamanho()];
+
+                        int qtd = 0;
+                        double soma = 0;
+                        for(int n = 0; n < (minhaPilha.Tamanho() + 1); n++) { 
+                            dados[n] = new Funcionario();
+                            valor = minhaPilha.Desempilha();
+                            dados[n] = valor;
+                            soma = valor.salario + soma;
+                            qtd++;
+                            }
+                        
+                       Console.WriteLine("A soma é : "+ soma);
+                        for(int empilha = 0; empilha < qtd; empilha++) 
+                        {
+                            minhaPilha.Empilha(dados[empilha]);
+                        }
+                    }
+                     
+
+                }
+                catch (Exception erro)
+                {
+                    Console.WriteLine("ERRO: " + erro.Message);
+                }
+            }
+            while (opcao != 9);
+        }
+    }
+}
---
+++ Exercicio 2/Exercicio 1e2 EstruturaDados/Funcionario.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio_1e2_EstruturaDados
+{
+    public class Funcionario
+    {
+        public string nome;
+        public double salario;
+    }
+}
